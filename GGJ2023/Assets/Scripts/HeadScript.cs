d6c1c07cf4fa8cc6445b5bfdb1dcf73df9fcd779
hucker48 <yuval.shai48@gmail.com>
2023-02-03 00:06:50 +0200

    basic prefab movement

---
+++ GGJ2023/Assets/Scripts/HeadScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadScript : MonoBehaviour
+{
+	private float horizontal;
+    
+    [SerializeField]
+	private float forwardSpeed; 
+    [SerializeField]
+	private float angularSpeed; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		horizontal = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate() {
+		transform.Translate(Vector2.up * forwardSpeed * Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * - horizontal * angularSpeed * Time.fixedDeltaTime);
+
+    }
+}
--- GGJ2023/Assets/Scripts/TailScript.cs
+++ GGJ2023/Assets/Scripts/TailScript.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TailScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float pointSpacing = 0.1f;
+	private LineRenderer line;
+
+	private List<Vector2> points;
+	private EdgeCollider2D col;
+	[SerializeField]
+	private bool isDrawing = true;
+	private HeadScript head;
+
+	
+	void Start()
+	{
+		line = GetComponent<LineRenderer>();
+		col = GetComponent<EdgeCollider2D>();
+		head = GetComponentInParent<HeadScript>();
+		
+		points = new List<Vector2>();
+	}
+
+	void Update()
+	{
+		if (points.Count <= 0)
+		{
+			SetPoint();
+		}
+		if (!line.enabled)
+		{
+			line.enabled = true;
+		}
+		if (!isDrawing)
+		{
+			return;
+		}
+		if (Vector2.Distance(points.Last(), head.transform.position) > pointSpacing)
+		{
+			SetPoint();
+		}
+	}
+
+	void FixedUpdate() {
+		
+	}
+
+	void SetPoint()
+	{
+		if (points.Count > 1)
+		{
+			col.points = points.ToArray<Vector2>();
+		}
+		points.Add(head.transform.position);
+		line.positionCount = points.Count;
+		line.SetPosition(points.Count - 1, head.transform.position);
+	}
+
+	// public void DisableDrawing()
+	// {
+	// 	isDrawing = false;
+	// 	StartCoroutine(SynchronizeCollider());
+	// }
+
+	// IEnumerator SynchronizeCollider()
+	// {
+	// 	yield return new WaitForSeconds(0.2f);
+
+	// 	if (points.Count > 1)
+	// 	{
+	// 		col.points = points.ToArray<Vector2>();
+	// 	}
+	// }
 }
