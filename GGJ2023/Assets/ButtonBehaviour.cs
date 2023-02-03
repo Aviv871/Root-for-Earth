3eacb2f85bc883838c2bf2a74ddf4887328c47d6
Liad <Liadharduf@gmail.com>
2023-02-03 03:02:35 +0200

    add main menu

---
+++ GGJ2023/Assets/ButtonBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("YuvalAndAvivScene");
+    }
+
+    public void Instructions()
+    {
+        // LoadScene();
+    }
+
+    public void Scoreboard()
+    {
+        // LoadScene();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+}
