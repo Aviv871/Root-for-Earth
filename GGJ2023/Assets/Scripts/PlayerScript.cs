8e7976566142dd929844513b7cdd993ec008d863
Reuven Karasik <rubik@karasik.org>
2023-02-03 02:21:21 +0200

    git merge

--- GGJ2023/Assets/Scripts/PlayerScript.cs
+++ GGJ2023/Assets/Scripts/PlayerScript.cs
@@ -7,8 +7,7 @@ public class PlayerScript : MonoBehaviour
 
     public Transform headTransform;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         headTransform = GetComponentInChildren<HeadScript>().transform;
     }
